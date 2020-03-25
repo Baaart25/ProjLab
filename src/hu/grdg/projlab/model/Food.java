7c04a9470cb5a3531a21f852e0b79dbdda52952a
barrow099 <mate@barrow099.hu>
2020-03-25 15:15:46 +0100

    asfdfsd

--- src/hu/grdg/projlab/model/Food.java
+++ src/hu/grdg/projlab/model/Food.java
@@ -4,4 +4,8 @@ package hu.grdg.projlab.model;
  * An Item that increases the players body temp when used
  */
 public class Food extends Item{
+
+    public void eat() {
+        //////////
+    }
 }
