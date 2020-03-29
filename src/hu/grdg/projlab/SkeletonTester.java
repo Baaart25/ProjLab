0d7fd3fcfc86c48544828ab45bfeda786bc2ee77
Barrow099 <mate@barrow099.hu>
2020-03-29 15:30:54 +0200

    Added docs to SkeletonTester

--- src/hu/grdg/projlab/SkeletonTester.java
+++ src/hu/grdg/projlab/SkeletonTester.java
@@ -2,16 +2,31 @@ package hu.grdg.projlab;
 
 import java.util.*;
 
+/**
+ * Testing utility class for Skeleton
+ * @author barrow099
+ */
 public class SkeletonTester {
 
+    //The available tests
     private static HashMap<String, Runnable> skeletonTests = new HashMap<>();
+
+    /**
+     * Register a new test function to the system
+     * @param name The name of the test, this will be displayed to the user
+     * @param r The test function, a void parameterless function
+     */
     public static void registerTest(String name, Runnable r) {
         skeletonTests.put(name, r);
     }
 
+    /**
+     * Starts the testing, loops until exit
+     * Displays the test cases to the user to choose and executes the chosen test
+     */
     public static void start() {
         while(true) {
-            System.out.println("\n\n");
+            System.out.println("\n");
             System.out.println("\t0: Kilépés");
 
             String[] tests = skeletonTests.keySet().toArray(new String[skeletonTests.keySet().size()]);
@@ -22,27 +37,180 @@ public class SkeletonTester {
             int tnum = SkeletonTester.readNumber();
             if(tnum >= 0  && tnum <= tests.length) {
                 if(tnum == 0) {
+                    //Exit
                     break;
                 }else {
                     skeletonTests.get(tests[tnum - 1]).run();
                 }
             }
         }
+        System.out.println("Goodbye");
     }
 
+
+
     //<editor-fold defaultstate="collapsed" desc="Skeleton logic">
+
+    /*
+     * The following code is used for printing the function call tree
+     * and provides other features such as numbed and yesno input
+     * Please be careful when modifying :D
+     */
+
+
     //Current skeleton test env
     private static SkeletonTester currentInstance = null;
-
-    //Instance variables
-    private String name;
-    private HashMap<Object, String> nameMap;
-    private int fnLevel = 0;
+    //Scanner for input functions
     private static Scanner scanner = new Scanner(System.in);
 
+    //Instance variables
+    private String testName;
+    //Current named objects
+    private HashMap<Object, String> nameMap;
+    //Current call tree level
+    private int fnLevel = 0;
+
+
     private SkeletonTester(String name) {
-        this.name = name;
+        this.testName = name;
         nameMap = new HashMap<>();
+    }
+
+    //Static methods
+
+    /**
+     * Registers a name for a Object
+     * The name will be used when te tester prints this instance
+     * @param o The object
+     * @param name The name
+     */
+    public static void addNamedReference(Object o, String name) {
+        checkInstance();
+        currentInstance.nameMap.put(o, name);
+    }
+
+    /**
+     * Signals a function call to the testing system
+     * The function name will be retrieved from the call stack
+     * @param instance The instance which method was called
+     * @param args Additional arguments to the method
+     */
+    public static void call(Object instance, Object ... args) {
+        checkInstance();
+        currentInstance.fnLevel++;
+        StackTraceElement[] stackTrace = Thread.currentThread().getStackTrace();
+        try {
+            StackTraceElement ste = stackTrace[2];
+            String method = ste.getMethodName();
+            String clazz = ste.getClassName();
+            printf("%s%s.%s(%s)", padding(), objectInfo(instance),method, String.join(", ",objaInfo(args)));
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Generates a padding string for the function call tree level
+     * @return The padding string
+     */
+    private static String padding() {
+        return " ".repeat(3).repeat(currentInstance.fnLevel);
+    }
+
+
+    /**
+     * Gets every objects info in an object array
+     * @param args The input object array
+     * @return Array of strings, the values are made using objectInfo()
+     */
+    private static String[] objaInfo(Object[] args) {
+        checkInstance();
+        if(args == null)
+            return new String[]{""};
+        String[] adata = new String[args.length];
+        for (int i = 0; i < args.length; i++) {
+            adata[i] = objectInfo(args[i]);
+        }
+
+        return adata;
+    }
+
+    /**
+     * Gets the registered name of the object, if its not available
+     * Objects.toString is used
+     * @param obj The object
+     * @return The name or toString value
+     */
+    private static String objectInfo(Object obj) {
+        checkInstance();
+        if(currentInstance.nameMap.containsKey(obj)) {
+            return currentInstance.nameMap.get(obj);
+        }else {
+            return Objects.toString(obj);
+        }
+    }
+
+    /**
+     * Begins a new test, creates the required instance and sets up the testing
+     * @param name The display name of the test
+     */
+    public static void beginTest(String name) {
+        currentInstance = new SkeletonTester(name);
+        printf("Skeleton testing started: %s", name);
+    }
+
+    /**
+     * Ends the current test
+     */
+    public static void endTest() {
+        checkInstance();
+        currentInstance = null;
+        printf("Testing ended");
+    }
+
+    /**
+     * Signal return from method call
+     */
+    public static void creturn() {
+        checkInstance();
+        currentInstance.fnLevel--;
+    }
+
+    /**
+     * Signal value return from the function
+     * It can be used to signal return from method call
+     * @param retVal The return value, if added as named reference it's name will be printed
+     */
+    public static void creturn(Object retVal) {
+        checkInstance();
+        currentInstance.fnLevel++;
+        printf("%sreturn %s", padding(), objectInfo(retVal));
+        currentInstance.fnLevel--;
+        currentInstance.fnLevel--;
+    }
+
+    /**
+     * Checks if in a test,
+     * @throws IllegalStateException if not
+     */
+    private static void checkInstance() {
+        if(currentInstance == null)
+            throw new IllegalStateException("Instance is null");
+    }
+
+    //Other utility functions
+    public static boolean askYesNo(String message) {
+        System.out.print(message);
+        System.out.print("[Y/N]: ");
+        do {
+            String line = scanner.nextLine();
+            if(line.toLowerCase().equals("y")) {
+                return true;
+            }else if(line.toLowerCase().equals("n")) {
+                return false;
+            }
+        }while (true);
+
     }
 
     public static int askNumber(String message) {
@@ -59,56 +227,6 @@ public class SkeletonTester {
         return scanner.nextInt();
     }
 
-    //Static methods
-    public static void addNamedReference(Object o, String name) {
-        checkInstance();
-        currentInstance.nameMap.put(o, name);
-    }
-
-    public static void call(Object instance, Object ... args) {
-        checkInstance();
-        currentInstance.fnLevel++;
-        StackTraceElement[] stackTrace = Thread.currentThread().getStackTrace();
-        try {
-            StackTraceElement ste = stackTrace[2];
-            String method = ste.getMethodName();
-            String clazz = ste.getClassName();
-            printf("%s%s.%s(%s)", padding(), objectInfo(instance),method, String.join(", ",objaInfo(args)));
-        }catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    private static String padding() {
-        StringBuilder sb = new StringBuilder();
-        for(int i = 0; i < currentInstance.fnLevel; i++) {
-            for(int x = 0; x < 3; x++)
-                sb.append(" ");
-        }
-        return sb.toString();
-    }
-
-    private static String[] objaInfo(Object[] args) {
-        checkInstance();
-        if(args == null)
-            return new String[]{""};
-        String[] adata = new String[args.length];
-        for (int i = 0; i < args.length; i++) {
-            adata[i] = objectInfo(args[i]);
-        }
-
-        return adata;
-    }
-
-    private static String objectInfo(Object obj) {
-        checkInstance();
-        if(currentInstance.nameMap.containsKey(obj)) {
-            return currentInstance.nameMap.get(obj);
-        }else {
-            return Objects.toString(obj);
-        }
-    }
-
     public static void print(Object o) {
         if(o == null)
             System.out.println("null");
@@ -118,49 +236,6 @@ public class SkeletonTester {
 
     public static void printf(String fmt, Object ... args) {
         print(String.format(fmt, args));
-    }
-
-    public static void beginTest(String name) {
-        currentInstance = new SkeletonTester(name);
-        printf("Skeleton testing started: %s", name);
-    }
-
-    public static void endTest() {
-        checkInstance();
-        currentInstance = null;
-        printf("Testing ended");
-    }
-
-    public static void creturn() {
-        checkInstance();
-        currentInstance.fnLevel--;
-    }
-
-    public static void creturn(Object retVal) {
-        checkInstance();
-        currentInstance.fnLevel++;
-        printf("%sreturn %s", padding(), objectInfo(retVal));
-        currentInstance.fnLevel--;
-        currentInstance.fnLevel--;
-    }
-
-    private static void checkInstance() {
-        if(currentInstance == null)
-            throw new IllegalStateException("Instance is null");
-    }
-
-    public static boolean askYesNo(String message) {
-        System.out.print(message);
-        System.out.print("[Y/N]: ");
-        do {
-            String line = scanner.nextLine();
-            if(line.toLowerCase().equals("y")) {
-                return true;
-            }else if(line.toLowerCase().equals("n")) {
-                return false;
-            }
-        }while (true);
-
     }
     //</editor-fold>
 }
