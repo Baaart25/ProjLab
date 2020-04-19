3c23acccd663355876b317c6e642b37e4e7aadf9
Mate Magyar <webmaster@barrow099.hu>
2020-04-19 18:58:06 +0200

    Created ProtoIO with string constants

--- src/hu/grdg/projlab/ProtoIO.java
+++ src/hu/grdg/projlab/ProtoIO.java
@@ -1,1 +1,74 @@
 
+/**
+* Helper class for printing to the standard output
+* Also contains helper functions to read numbers, bools, etc. from the 
+* standard input.
+* @author Barrow099
+*/
+public class ProtoIO {
+  
+  /**
+  * Static class for all output messages as specified in the 8th document.
+  * @author Barrow099
+  */
+  public static class OutputMessages {
+    public static final String CLEARGAME_OUT = "Game cleared";
+    public static final String NEWTILE_OUT = "Tile created";
+    public static final String ERR_NAME_USED = "Name already used";
+    public static final String SETNEIGHBOUR_OUT = "Neighbours updated";
+    public static final String ERR_TILE_NOT_FOUND = "Tile not found";
+    public static final String SETNEIGHBOUR_ERR_USED = "Neighbour already set";
+    public static final String INVENTORY_OUT = "Inventory: [%s]";
+    public static final String ERR_PLAYER_NOT_FOUND = "Player not found";
+    public static final String INSPECT_OUT = "Tile: [%s]";
+    public static final String ERR_NOT_ON_TILE = "Not on tile";
+    public static final String REMOVESNOW_OUT = "Snow layer removed";
+    public static final String REMOVESNOW_ERR_NOT_ENOUGH = "Not enough snow";
+    public static final String ESKIMO_OUT = "Eskimo created";
+    public static final String SCIENTIST_OUT = "Scientist created";
+    public static final String ITEM_OUT = "Item created";
+    public static final String POLARBEAR_OUT = "Polarbear created";
+    public static final String PUTSNOW_OUT = "Snow layer added";
+    public static final String STEP_OUT_TILE = "Stepped to tile";
+    public static final String STEP_OUT_HOLE = "Fall in hole";
+    public static final String STEP_OUT_UNSTABLE_DIE = "Died on unstable. Game ended";
+    public static final String STEP_BEAR_EAT = "Polarbear eats player. Game ended";
+    public static final String ERR_NEIGHBOUR_NOT_FOUND = "Neighbour not found";
+    public static final String LOAD_OUT = "Game state loaded";
+    public static final String LOAD_ERR_NOTFOUND = "File not found";
+    public static final String LOAD_ERR_FORMAT = "Formate error: %s";
+    public static final String ERR_IO = "IO error";
+    public static final String STAT_OUT = "ERRR -- DONT FUCKIN USE THIS -- ERRR";
+    public static final String SNOWSTORM_OUT_NORMAL = "Snowstorm done";
+    public static final String SNOWSTORM_OUT_PLAYERDIE = "Player died. Game ended";
+    public static final String SNOWSTORM_OUT_PLAYERDAMAGE = "Player damaged";
+    public static final String ADD_OUT = "Item added. Slot: %d";
+    public static final String USEITEM_OUT = "Item used";
+    public static final String USEITEM_ERR_NOITEM = "No such item";
+    public static final String USEITEM_ERR_ITEMFAIL = "Item use failed";
+    public static final String PICKUP_OUT = "Pickup successful";
+    public static final String PICKUP_ERR_FORZEN = "Item frozen";
+    public static final String ERR_SNOW = "Snow on tile";
+    public static final String ERR_NOITEM = "No item on tile";
+    public static final String SPECAB_OUT_ESKIMO = "Igloo built";
+    public static final String SPECAB_OUT_SCI = "Tile scanned: %d";
+    public static final String SPECAB_ERR_FAILURE = "SpecialAbility failure";
+    public static final String UNFREEZE_OUT = "Item unfreezed";
+  }
+  
+  /**
+  * Prints the message to the standard output
+  * @author Barrow099
+  */
+  public static void output(String message) {
+    throw new RuntimeException("Not implemented yet");
+  }
+  
+  /**
+  * Prints the formatted message to the standard output
+  * @author Barrow099
+  */
+  public static void outputf(String message, Object ... args) {
+    throw new RuntimeException("Not implemented yet");
+  }
+}
