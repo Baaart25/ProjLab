97714cbb390ed02a74095f9bcb842fcbfd728c50
Gergely Hevesi <hevesi.gergely@gmail.com>
2020-04-21 15:59:31 +0200

    Eskimo's scpecialAbility finished

--- src/hu/grdg/projlab/model/Eskimo.java
+++ src/hu/grdg/projlab/model/Eskimo.java
@@ -1,4 +1,13 @@
 package hu.grdg.projlab.model;
 
 public class Eskimo extends Player{
+
+    /**
+     * Calls buildIgloo on currentTile
+     * @return Succesfulness of operation
+     * @author Geri
+     */
+    public boolean specialAbility(){
+        return getCurrentTile().buildIgloo();
+    }
 }
