5298d3266170b93ad081a0123fedb636f049b68a
Dorka <dorinapetra@gmail.com>
2020-04-21 12:15:26 +0200

    IceTile done

--- src/hu/grdg/projlab/model/IceTile.java
+++ src/hu/grdg/projlab/model/IceTile.java
@@ -1,4 +1,14 @@
 package hu.grdg.projlab.model;
 
 public class IceTile extends Tile{
+
+    /**
+     * Scans the limit of the Tile
+     * @return -1 (the IceTile is unlimited)
+     * @author Dorina
+     */
+    @Override
+    public int scanLimit() {
+        return -1;
+    }
 }
