d45f6d3a48ed37a3ca514444907af0a42aa68f89
Gergely Hevesi <hevesi.gergely@gmail.com>
2020-04-22 11:49:58 +0200

    PolarBear finished

--- src/hu/grdg/projlab/model/PolarBear.java
+++ src/hu/grdg/projlab/model/PolarBear.java
@@ -2,31 +2,53 @@ package hu.grdg.projlab.model;
 
 public class PolarBear extends Entity{
 
-    //FIXME
+    /**
+     * Steps in given direction
+     * @param direction Direction of step
+     * @author Geri
+     */
     @Override
     public void move(int direction) {
-
+        Tile stepTile = currentTile.getNeighbour(direction);
+        currentTile.removeEntity(this);
+        stepTile.acceptEntity(this);
+        stepTile.bearAttack();
     }
 
-    //FIXME
+    /**
+     * Does nothing, can't be damaged
+     * @param i Amount of damage
+     * @author Dorina, Geri
+     */
     @Override
     public void damage(int i) {
 
     }
 
-    //FIXME
+    /**
+     * Does nothing, cannot die
+     * @author Dorina, Geri
+     */
     @Override
     public void die() {
 
     }
 
-    //FIXME
+    /**
+     * Does nothing, cannot suffocate
+     * @author Dorina, Geri
+     */
     @Override
     public void fallInWater() {
 
     }
 
-    //FIXME
+    /**
+     * Does nothing, cannot be saved
+     * @param t Not relevant
+     * @return false
+     * @author Dorina, Geri
+     */
     @Override
     public boolean savedFromWater(Tile t) {
         return false;
