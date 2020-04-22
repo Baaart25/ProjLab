59c9f3c316cf3a80af5800346e5c9950e2ff435d
Hegedűs Dániel <hegeda114@gmail.com>
2020-04-22 17:16:48 +0200

    Tent és RemoveTent megvalósítása (kéne má Controller :( )

--- src/hu/grdg/projlab/model/RemoveTent.java
+++ src/hu/grdg/projlab/model/RemoveTent.java
@@ -1,4 +1,51 @@
 package hu.grdg.projlab.model;
 
+import java.util.ArrayList;
+
 public class RemoveTent implements TurnBasedEvent{
+    private static RemoveTent singleton;
+    private ArrayList<Tent> tents;
+
+    private RemoveTent(){ }
+
+    /**
+     * Értesít minden sátrat, hogy csökkentse a számlálóját (meghívja a decreaseLife() függvényüket)
+     * @param lvl az adott leve (csak az automatizált hóviharhoz kell)
+     * @param fullTurn akkor true, ha egy teljes kör telt le (ehhez lényegtelen)
+     */
+    void doEvent(Level lvl, boolean fullTurn) {
+        for (Tent tent : tents) {
+            tent.decreaseLife();
+        }
+    }
+
+    /**
+     * Visszaadja az egyetlen RemoveTent példányt amit statikus attribútumként tárol.
+     * @return egyetlen RemoveTent példány
+     * @author Dani
+     */
+    public static RemoveTent getInstance() {
+        if (singleton == null)
+            singleton = new RemoveTent();
+        return singleton;
+    }
+
+    /**
+     * Hozzáadja a paraméterként kapott sátrat a tárolt tents listához.
+     * @param tent a hozzáadandó sátor
+     * @author Dani
+     */
+    void addTent(Tent tent) {
+        singleton.tents.add(tent);
+        tent.setLife(3); //FIXME: meg kéne kapnia a controller-től a játékosok számát!
+    }
+
+    /**
+     * Eltávolítja a paraméterként kapott sátrat a tárolt tents listához.
+     * @param tent a listából kiveendő sátor
+     * @author Dani
+     */
+    void removeTent(Tent tent) {
+        singleton.tents.remove(tent);
+    }
 }
--- src/hu/grdg/projlab/model/Tent.java
+++ src/hu/grdg/projlab/model/Tent.java
@@ -1,12 +1,54 @@
 package hu.grdg.projlab.model;
 
 
-//OOOOOOOOOOOOOoooooooooooo
+
 //Ez nincs a doksiban xD
 public class Tent extends Item {
+    private int life;
 
-
+    /**
+     * Sátor használata:
+     * egy sátrat rak arra a mezőre ahol a tulajdonosa
+     * áll, és gondoskodik arról, hogy az adott Tent feliratkozásra kerüljön a
+     * RemoveTent osztályra.
+     * @return a sikerességével tér vissza (false ha már van sátor az adott mezőn)
+     * @author Dani
+     */
     public boolean useItem(){
-        return false;
+        Tile tile = owner.getCurrentTile();
+        boolean res = tile.buildTent();
+        if(res) {
+            RemoveTent.getInstance().addTent(this);
+        }
+        return res;
+    }
+
+    /**Csökkenti eggyel a sátor élettartamát
+     * @author Dani
+     */
+    public void decreaseLife() {
+        life--;
+        if(life == 0) {
+            Tile tile = owner.getCurrentTile();
+            tile.removeTent();
+            RemoveTent.getInstance().removeTent(this);
+        }
+    }
+
+    /**EZ MÉG NEM SZEREPELT DOKSIBAN!
+     * Beállítja a paraméterként kapott értékre a sátor élettartamát
+     * @param life
+     * @author Dani
+     */
+    public void setLife(int life) {
+        this.life = life;
     }
 }
+
+/*Tent
+public boolean useItem(): egy sátrat rak arra a mezőre ahol a tulajdonosa
+áll, és gondoskodik arról, hogy az adott Tent feliratkozásra kerüljön a
+RemoveTent osztályra.
+public void decreaseLife(): csökkenti az adott Tent élettartamát, és
+amennyiben eléri az a nullát, meghívja a RemoveTent osztály removeTent(t:
+Tent) függvényét az adott tent-et paraméterként átadva annak.*/
