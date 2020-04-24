4f57d1de4528da4dcaea1eeaae28e72bab7810b1
barrow099 <mate@barrow099.hu>
2020-04-24 11:04:37 +0200

    Fixed ESKIMO_BUILD_IGLOO_ON_EMPTY_TILE_EXPECTED

--- src/hu/grdg/projlab/util/commands/SpecialAbilityCommand.java
+++ src/hu/grdg/projlab/util/commands/SpecialAbilityCommand.java
@@ -15,7 +15,10 @@ public class SpecialAbilityCommand extends Command {
     @Override
     public void runCommand(ProtoRuntime state, List<String> inputParams) throws CommandException {
         String pName = getStringArg(inputParams);
-        int dir = getIntArg(inputParams);
+
+        int dir = 0;
+        if(inputParams.size() > 0 && !inputParams.get(0).equals("null"))
+          dir = getIntArg(inputParams);
 
         Player p;
         if((p = getPlayer(pName, state)) == null) {
