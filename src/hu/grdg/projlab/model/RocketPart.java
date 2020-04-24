dd80d6c95b2051f6a291627d940b9ac153ace424
Botond Barta <b.botond25@gmail.com>
2020-04-24 12:32:10 +0200

    addplayer to controller

--- src/hu/grdg/projlab/model/RocketPart.java
+++ src/hu/grdg/projlab/model/RocketPart.java
@@ -3,7 +3,7 @@ package hu.grdg.projlab.model;
 public class RocketPart extends Item{
     private Controller controller;
 
-
+    
     public boolean useItem(){
         return controller.checkWin();
     }
