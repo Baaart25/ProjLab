eaa1ebd98a5d1d2082617ceef27f46890efbfe41
Gergely Hevesi <hevesi.gergely@gmail.com>
2020-05-11 17:50:29 +0200

    ScientistSelected

--- src/hu/grdg/projlab/gui/render/ScientistRenderer.java
+++ src/hu/grdg/projlab/gui/render/ScientistRenderer.java
@@ -14,23 +14,30 @@ public class ScientistRenderer implements EntityRenderer {
     private BufferedImage scientistOnHoleImage;
     private BufferedImage scientistOnHoleSelectedImage;
 
-    public ScientistRenderer(){
+    public ScientistRenderer() {
         try {
             this.scientistImage = ImageIO.read(new File("img/Scientist.png"));
             this.scientistSelectedImage = ImageIO.read(new File("img/ScientistSelected.png"));
             this.scientistOnHoleImage = ImageIO.read(new File("img/ScientistOnHoleTile.png"));
             this.scientistOnHoleSelectedImage = ImageIO.read(new File("img/ScientistOnHoleTileSelected.png"));
-        } catch (IOException e){
+        } catch (IOException e) {
             e.printStackTrace();
             System.exit(-1);
         }
     }
 
     @Override
-    public void draw(Graphics2D g, boolean isActive, int xOffset, int yOffset, boolean isInWater){
-        if(isInWater)
-            g.drawImage(scientistOnHoleImage,xOffset,yOffset,25,25,null);
-        else
-            g.drawImage(scientistImage, xOffset, yOffset,25, 25, null);
+    public void draw(Graphics2D g, boolean isActive, int xOffset, int yOffset, boolean isInWater) {
+        if (isInWater)
+            if (isActive) {
+                g.drawImage(scientistOnHoleSelectedImage, xOffset, yOffset, 25, 25, null);
+            } else {
+                g.drawImage(scientistOnHoleImage, xOffset, yOffset, 25, 25, null);
+            }
+        else if (isActive) {
+            g.drawImage(scientistSelectedImage, xOffset, yOffset, 25, 25, null);
+        } else {
+            g.drawImage(scientistImage, xOffset, yOffset, 25, 25, null);
+        }
     }
 }
