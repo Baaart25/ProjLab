2bca77af238b6bd52fe28898b011052359f88e1f
Botond Barta <b.botond25@gmail.com>
2020-05-11 14:39:19 +0200

    breakableshovelrenderer class

---
+++ src/hu/grdg/projlab/gui/render/BreakableShovelRenderer.java
@@ -0,0 +1,13 @@
+package hu.grdg.projlab.gui.render;
+
+import hu.grdg.projlab.gui.ItemRenderer;
+
+import java.awt.*;
+
+public class BreakableShovelRenderer implements ItemRenderer {
+    //TODO
+    @Override
+    public void draw(Graphics2D g) {
+
+    }
+}
