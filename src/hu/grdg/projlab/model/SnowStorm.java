346aca48135f79b961d4d19c60f7d2901c6ac263
Dorka <dorinapetra@gmail.com>
2020-05-11 16:13:01 +0200

    SnowStorm done

--- src/hu/grdg/projlab/model/SnowStorm.java
+++ src/hu/grdg/projlab/model/SnowStorm.java
@@ -1,6 +1,7 @@
 package hu.grdg.projlab.model;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 public class SnowStorm implements TurnBasedEvent{
     private static SnowStorm instance = null;
@@ -18,9 +19,25 @@ public class SnowStorm implements TurnBasedEvent{
         instance = this;
     }
 
+
+    /**
+     * Choose the tiles where the SnowStorm happens and do the storm
+     * @param lvl the level of the game
+     * @param fullTurn true if the storm has to be happen
+     * @author Dorina
+     */
     @Override
     public void doEvent(Level lvl, boolean fullTurn) {
-        ArrayList<Tile> tiles =lvl.getTiles();
+        if(fullTurn) {
+            ArrayList<Tile> tiles = lvl.getTiles();
+            Random rnd = new Random();
+            int x = rnd.nextInt(7), y = rnd.nextInt(7);
+            for (int i = 0; i < 3; i++) {
+                for (int j = 0; j < 3; j++) {
+                    doStorm(tiles.get(x + i + (y + j) * 10), rnd.nextInt(3));
+                }
+            }
+        }
 
     }
 
