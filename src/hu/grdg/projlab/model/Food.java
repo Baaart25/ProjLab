9d4c863cd1a2600f9e9186ea67bc3f4e684b90c3
Botond Barta <b.botond25@gmail.com>
2020-05-17 16:45:24 +0200

    food comment

--- src/hu/grdg/projlab/model/Food.java
+++ src/hu/grdg/projlab/model/Food.java
@@ -3,7 +3,12 @@ package hu.grdg.projlab.model;
 import hu.grdg.projlab.gui.ItemRenderer;
 import hu.grdg.projlab.gui.render.FoodRenderer;
 
+/**
+ * Food, ha használják, akkor életet ad az ownerjének
+ * Itemből származik
+ */
 public class Food extends Item{
+    //tárolja a hozzátartozó renderert
     private static FoodRenderer renderer = new FoodRenderer();
 
     /**
