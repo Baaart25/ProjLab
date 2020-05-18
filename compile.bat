61edaef8ce3971779946d8c57ad89a741e7e7d5f
barrow099 <mate@barrow099.hu>
2020-05-18 11:55:41 +0200

    Added compile scripts

--- compile.bat
+++ compile.bat
@@ -2,3 +2,5 @@
 rd /S /Q out
 dir /S /B "src\*.java" > sources.txt
 javac -d out @sources.txt
+cp src\META-INF\MANIFEST.MF out
+xcopy /E /I img out\
--- compile.sh
+++ compile.sh
@@ -2,3 +2,5 @@
 rm -rf out
 find src -name "*.java" > sources.txt
 javac -d out @sources.txt
+cp src/META-INF/MANIFEST.MF out
+cp -r img out/
